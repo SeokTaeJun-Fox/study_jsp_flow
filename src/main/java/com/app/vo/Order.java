789266b50e69fe7973b4fca0568259ddc3d5d593
SeokTaeJun-Fox <moderneuropean81@gmail.com>
2026-03-26 17:57:30 +0900

    feat:flow

---
+++ src/main/java/com/app/vo/Order.java
@@ -0,0 +1,74 @@
+package com.app.vo;
+
+import java.util.Objects;
+
+public class Order {
+	private Long id;
+	private Integer orderCount;
+	private Integer productId;
+	private Integer memberId;
+	
+	public Order() {;}
+
+	public Order(Long id, Integer orderCount, Integer productId, Integer memberId) {
+		this.id = id;
+		this.orderCount = orderCount;
+		this.productId = productId;
+		this.memberId = memberId;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getOrderCount() {
+		return orderCount;
+	}
+
+	public void setOrderCount(Integer orderCount) {
+		this.orderCount = orderCount;
+	}
+
+	public Integer getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+
+	public Integer getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(Integer memberId) {
+		this.memberId = memberId;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", orderCount=" + orderCount + ", productId=" + productId + ", memberId=" + memberId
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
+	}
+}
